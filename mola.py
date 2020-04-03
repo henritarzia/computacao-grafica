b9614095cb1bc0c3a6012df4c5e06c02c5d4e201
henritarzia <henritarzia@gmail.com>
2020-04-02 22:16:40 -0300

    Correção do retorno do pulo

--- mola.py
+++ mola.py
@@ -18,7 +18,7 @@ import math
 
 glfw.init()
 glfw.window_hint(glfw.VISIBLE, glfw.FALSE);
-window = glfw.create_window(800, 800, "Mola 2D", None, None)
+window = glfw.create_window(1000, 1000, "Mola 2D", None, None)
 glfw.make_context_current(window)
 
 """Vertex Shader"""
@@ -82,34 +82,23 @@ glUseProgram(program)
 #Inicializa gerador de números aleatórios
 rand.seed(a=None, version=2)
 
+#Coordenadas de cada vértice
+points = []   #lista de pontos da mola
+x = 0         #coordenada x
+y = 0         #coordenada y
+
+#Obtém os pontos da mola por meio da função sin(24*a)/10
+for a in np.arange(-186,192,6):
+    y = math.radians(a)/24
+    x = math.sin(24*a)/10
+    c = (x,y)
+    points.append(c)  
+
+size = len(points)    #número de vértices da mola
+
 #Prepara espaço para 21 vértices usando 2 coordenadas (x,y) -- vértices da mola
-vertices = np.zeros(21, [("position", np.float32, 2)])
-
-#Calcula as coordenadas de cada vértice
-
-inc_x = 0.0         #posição central da mola no eixo x 
-inc_y = -0.6        #posição do vértice inicial da mola no eixo y
-dist_x = 0.08       #distância do vértice em relação ao centro da mola na coordernada x
-dist_y = 0.012      #valor do vértice em relação ao eixo y
-side = 1            #variável auxiliar para identificação do valor da coordenada x em vértices ímpares
-
-x = 0
-y = 0
-
-for i in np.arange(21):
-    if i % 2 == 0: 
-        x = inc_x
-    else: 
-        x = inc_x + (side * dist_x)
-        side *= -1
-
-    #define coordernadas do vértice
-    y = i * dist_y + inc_y
-    vertices['position'][i] = [x,y]     
-
-xf = vertices['position'][0][0]     #armazena a coordenada x do vértice inicial
-yf = vertices['position'][0][1]     #armazena a coordenada y do vértice inicial
-size_y = vertices['position'][20][1] - vertices['position'][0][1]
+vertices = np.zeros(size, [("position", np.float32, 2)])
+vertices['position'] = np.array(points)
 
 #Requisita um slot de buffer da GPU
 buffer = glGenBuffers(1)
@@ -137,10 +126,20 @@ glVertexAttribPointer(loc, 2, GL_FLOAT, False, stride, offset)
 s_y = 1.0          #escala para deformação
 release = True     #indicador de aplicação de compressão (False) ou descompressão (True) 
 jump = False       #indicador de salto 
+returning = False  #indicador de retorno à posição central 
 direction = 0      #direção do salto
 
+xs = points[0][0]       #x de referência para a escala
+ys = points[0][1]       #y de referência para a escala
+
+yr = points[int(size/2)][1]    #y de referência para a rotação (vértice médio da mola)
+xr = 0                         #x de referência para a rotação
+
+xt = 0
+
 def key_event(window,key,scancode,action,mods):
-    global s_y, release, jump, direction
+    global s_y, direction, xr, xt
+    global release, jump, returning
 
     #Ao pressionar a seta para baixo, é exercida força de compressão na mola
     if key == 264 and action != 0 and jump == False: release = False
@@ -150,8 +149,11 @@ def key_event(window,key,scancode,action,mods):
     if not release and s_y >= 0.16: s_y -= 0.02
     #Caso a tecla tenha sido solta e a mola apresente o valor mínimo de compressão, indica ocorrência de salto
     if release and s_y <= 0.3: 
-        jump = True                      #ativa flag de salto
-        direction = rand.choice([-1,1])  #define a direção do salto da mola (1: lado esquerdo / -1: lado direito)
+        jump = True                          #ativa flag de salto
+        if not returning:                 
+            direction = rand.choice([-1,1])  #define a direção do salto da mola (1: lado esquerdo / -1: lado direito)
+            xr = 0.4 * (-direction)
+        else: xr = 0.4 * direction             
 
 glfw.set_key_callback(window,key_event)
 
@@ -161,14 +163,10 @@ glfw.show_window(window)
 
 """Loop principal"""
 
-t_x = 0.0       #deslocamento no eixo x
-t_y = 0.0       #deslocamento no eixo y
 angle = 0.0     #ângulo da etapa do salto
-frame = 0       #contador da animação de salto
-pos = 0         #posição da mola
-
 sin = 0.0       #seno do ângulo 
 cos = 1.0       #cosseno do ângulo
+speed = 2.0     #incremento no ângulo a cada iteração
 
 def multiplica_matriz(a,b):
     m_a = a.reshape(4,4)
@@ -180,31 +178,20 @@ def multiplica_matriz(a,b):
 while not glfw.window_should_close(window):
     #Altera variáveis para realização do salto
     if jump and s_y == 1.0:
-        #cálculo da posição
-        pos = int(frame/10) + 1
-        #cálculo do deslocamento
-        t_x = -direction * pos * 0.15
-        t_y = 0.4
-        #cálculo do ângulo atual
-        angle = direction * 45 * pos
-        cos = math.cos( math.radians(angle) )
-        sin = math.sin( math.radians(angle) )
-        #incremento do contador
-        frame += 1
-
-    #Correção da coordenada y na posição final do movimento
-    if pos == 4: t_y = size_y
+        sin = math.sin(math.radians(angle))
+        cos = math.cos(math.radians(angle)) 
+        if not returning: angle += speed * direction
+        else: angle -= speed * direction 
 
     #Retorna a mola à sua posição original
-    if jump and frame == 40:
+    if jump and (angle == 180 + speed or angle == -(180 + speed)):
         jump = False
-        direction = 0
-        pos = 0
-        frame = 0
+        returning = not returning 
+        angle = 0
+        sin = 0.0
         cos = 1.0
-        sin = 0.0 
-        t_x = 0.0
-        t_y = 0.0
+        if returning: xt = 2 * xr 
+        else: xt = 0 
 
     glfw.poll_events() 
 
@@ -220,15 +207,26 @@ while not glfw.window_should_close(window):
                                0.0,  0.0, 0.0, 1.0], np.float32)
     
     #Matrizes para manutenção do ponto de referência (base) da mola
-    mat_reference_before = np.array([  1.0, 0.0, 0.0, xf, 
-                                       0.0, 1.0, 0.0, yf, 
-                                       0.0, 0.0, 1.0, 0.0, 
-                                       0.0, 0.0, 0.0, 1.0], np.float32)
+    mat_reference_before_s = np.array([  1.0, 0.0, 0.0, xs, 
+                                         0.0, 1.0, 0.0, ys, 
+                                         0.0, 0.0, 1.0, 0.0, 
+                                         0.0, 0.0, 0.0, 1.0], np.float32)
 
-    mat_reference_after = np.array([  1.0, 0.0, 0.0, -xf, 
-                                      0.0, 1.0, 0.0, -yf, 
-                                      0.0, 0.0, 1.0, 0.0, 
-                                      0.0, 0.0, 0.0, 1.0], np.float32)
+    mat_reference_after_s = np.array([  1.0, 0.0, 0.0, -xs, 
+                                        0.0, 1.0, 0.0, -ys, 
+                                        0.0, 0.0, 1.0, 0.0, 
+                                        0.0, 0.0, 0.0, 1.0], np.float32)
+
+    #Matrizes para manutenção do ponto de referência (base) da mola
+    mat_reference_before_r = np.array([  1.0, 0.0, 0.0, xr, 
+                                         0.0, 1.0, 0.0, yr, 
+                                         0.0, 0.0, 1.0, 0.0, 
+                                         0.0, 0.0, 0.0, 1.0], np.float32)
+
+    mat_reference_after_r = np.array([  1.0, 0.0, 0.0, -xr, 
+                                        0.0, 1.0, 0.0, -yr, 
+                                        0.0, 0.0, 1.0, 0.0, 
+                                        0.0, 0.0, 0.0, 1.0], np.float32)
 
     #O eixo y corresponde ao único passível de modificação do coeficiente
     #de escala por causa da deformação sofrida (compressão da mola) 
@@ -237,19 +235,19 @@ while not glfw.window_should_close(window):
                             0.0, 0.0, 1.0, 0.0,
                             0.0, 0.0, 0.0, 1.0], np.float32)
 
-    #Matriz de translação
-    T = np.array([  1.0, 0.0, 0.0, t_x, 
-                    0.0, 1.0, 0.0, t_y, 
+    #Matrizes para manutenção do ponto de referência (base) da mola
+    T = np.array([  1.0, 0.0, 0.0, xt, 
+                    0.0, 1.0, 0.0, 0.0, 
                     0.0, 0.0, 1.0, 0.0, 
                     0.0, 0.0, 0.0, 1.0], np.float32)
     
     #Matriz de escala com ponto de referência
-    S = multiplica_matriz(mat_reference_before, mat_scale)
-    S = multiplica_matriz(S, mat_reference_after)
+    S = multiplica_matriz(mat_reference_before_s, mat_scale)
+    S = multiplica_matriz(S, mat_reference_after_s)
 
     #Matriz de rotação com ponto de referência
-    R = multiplica_matriz(mat_reference_before, mat_rotation)
-    R = multiplica_matriz(R, mat_reference_after)
+    R = multiplica_matriz(mat_reference_before_r, mat_rotation)
+    R = multiplica_matriz(R, mat_reference_after_r)
 
     mat_transform = multiplica_matriz(R,S)
     mat_transform = multiplica_matriz(T,mat_transform)
